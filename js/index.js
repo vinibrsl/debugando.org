b5d6018e31f3be8b815f097a478820f25c90c67a
Vinicius Brasil <vnbrs@icloud.com>
2019-05-01 14:00:32 -0300

    Create blog boileerplate using Jekyll

--- js/index.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    // dynamically add e-mail to avoid plain crawling
-    var emailElement = document.querySelector("#email-link");
-    emailElement.href = "mailto:me@vnbrs.com";
-    emailElement.innerHTML = "me@vnbrs.com";
-};
